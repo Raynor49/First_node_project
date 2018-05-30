a729090b9d0a17d8e79aa19a5d04f9fd7107262c
Willis Roberts <willisroberts49@gmail.com>
2018-05-30 13:42:21 -0400

    animal_fun takes letter from process, reads animals.txt and writes all animals which start with letter to a new file

--- animal_fun.js
+++ animal_fun.js
@@ -4,5 +4,21 @@ fs.readFile('./animals.txt', 'utf-8', (err, data) => {
     console.log(err)
     return
   }
-  console.log(data)
+  const letter = process.argv[2]
+  let animals = data.split('\n')
+  let filtered_animals = []
+
+  for (var i = 0; i < animals.length; i++) {
+      if (animals[i].startsWith(letter) && animals[i] !== 'list'){
+        filtered_animals.push(animals[i])
+      }
+  }
+
+  fs.writeFile(`${letter}_animals.txt`, filtered_animals.join('\n'), (err, data) => {
+    if (err) {
+      console.log(err)
+      return
+    }
+    console.log('it worked!')
+  })
 })
