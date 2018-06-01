03267cdb6f23be9fa3d9f0ce2cfca2f6304ff374
Willis Roberts <willisroberts49@gmail.com>
2018-06-01 15:43:11 -0400

    gitserver

---
+++ first_project/github_grabber.js
@@ -0,0 +1,11 @@
+const fs = require('fs')
+const querystring = require('querystring')
+const https = require('https')
+const http = require('http')
+
+const githubServer = http.createServer((req, res) => {
+  if (req.method == 'POST'){
+    res.end("I'm a POST request")
+  }
+  res.end("Danger, not a POST request")
+})
